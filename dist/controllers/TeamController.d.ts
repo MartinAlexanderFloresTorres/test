ec1b93c8fee04ba9705e89cc13e34e0c4bad9938
Joao Victor Ferreira Carvalho <137827716+JoaoVictorFerrer@users.noreply.github.com>
2024-12-23 09:23:56 +0100

    tsconfig para deploy y eliminado dist

--- dist/controllers/TeamController.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from "express";
-export declare class TeamMemberController {
-    static getProjectTeam: (req: Request, res: Response) => Promise<void>;
-    static findMemberByEmail: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static addMemberById: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-    static removeMemberById: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
-}
