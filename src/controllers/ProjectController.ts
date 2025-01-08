18803c2ae98a36ce30d6871e18d289df1fc5182f
Joao Victor Ferreira Carvalho <137827716+JoaoVictorFerrer@users.noreply.github.com>
2025-01-08 09:11:57 +0100

    probando importaciones

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import Project from "../models/Project";
+import Project from "models/Project";
 
 
 
--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -1,6 +1,6 @@
 import type { Request,Response } from "express"
-import User from "../models/User"
-import Project from "../models/Project"
+import User from "models/User"
+import Project from "models/Project"
 
 export class TeamMemberController {
 
